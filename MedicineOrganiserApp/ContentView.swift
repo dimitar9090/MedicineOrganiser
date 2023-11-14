9058b636e63e8048e5ab09d5aef0e5f651913d49
Dimitar Anguelov <d.angelov.9090@gmail.com>
2023-11-14 15:46:09 +0200

    Initial Commit

---
+++ MedicineOrganiserApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedicineOrganiserApp
+//
+//  Created by Dimitar Angelov on 14.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedicineOrganiserApp/MedicineOrganiserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicineOrganiserAppApp.swift
+//  MedicineOrganiserApp
+//
+//  Created by Dimitar Angelov on 14.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MedicineOrganiserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
