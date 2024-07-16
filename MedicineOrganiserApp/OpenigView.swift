8358a04896adb685bb7722cdf95bdda8f9edf520
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-07-16 16:59:36 +0300

    i made opening view

---
+++ MedicineOrganiserApp/OpenigView.swift
@@ -0,0 +1,18 @@
+//
+//  OpenigView.swift
+//  MedicineOrganiserApp
+//
+//  Created by Dimitar Angelov on 16.07.24.
+//
+
+import SwiftUI
+
+struct OpenigView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpenigView()
+}
